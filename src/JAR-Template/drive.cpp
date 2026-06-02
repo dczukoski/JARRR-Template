869099dae6af1f28c08ccdc15d0f0e73e6856cf6
Davin Czukoski <davin@robotrevolution.net>
2026-06-02 18:10:28 -0400

    In void Drive::drive_distance added drive_stop(coast) after the while loop to make sure drive has come to a complete stop sometimes one side of the drive will continue its motion.

--- src/JAR-Template/drive.cpp
+++ src/JAR-Template/drive.cpp
@@ -321,6 +321,7 @@ void Drive::drive_distance(float distance, float heading, float drive_max_voltag
     drive_with_voltage(drive_output+heading_output, drive_output-heading_output);
     task::sleep(10);
   }
+  drive_stop(coast);
 }
 
 /**
